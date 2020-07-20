c22d4f525da4ed6c688989c071439221cbc7252a
djeffersonsilva <djeffersonsilva@vhsolucoes.com.br>
2020-07-20 20:50:32 -0300

    Updates

--- src/main/kotlin/br/com/djefferson/controller/ResourceController.kt
+++ src/main/kotlin/br/com/djefferson/controller/ResourceController.kt
@@ -1,15 +1,39 @@
 package br.com.djefferson.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import br.com.djefferson.model.Resource
+import br.com.djefferson.repository.ResourceRepository
+import org.apache.coyote.Response
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/resource")
-class ResourceController {
+@RequestMapping("/api/resources")
+class ResourceController(private val resourceRepository: ResourceRepository) {
+
+    @PostMapping
+    fun save(@RequestBody resource: Resource): ResponseEntity<Resource> {
+        return ResponseEntity.ok(resourceRepository.save(resource))
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteById(@PathVariable(value = "id") id: Long): ResponseEntity<Void> {
+        return resourceRepository.findById(id).map { resource ->
+            resourceRepository.delete(resource)
+            ResponseEntity<Void>(HttpStatus.OK)
+        }.orElse(ResponseEntity.notFound().build())
+    }
 
     @GetMapping
-    fun get(@RequestParam(value = "name") name: String) = mapOf("name" to name)
+    fun getAllResources(): List<Resource> {
+        return resourceRepository.findAll()
+    }
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable(value = "id") id: Long): ResponseEntity<Resource> {
+        return resourceRepository.findById(id).map { r ->
+            ResponseEntity.ok(r)
+        }.orElse(ResponseEntity.notFound().build())
+    }
 
 }
